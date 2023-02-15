638b21e157fca638404f267874ce1c0f77ab880f
Aaron Humphres <aaronhumphres@yahoo.com>
2023-02-15 14:55:11 -0700

    working reviews scrolling

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,8 +6,25 @@ const Home = (props) => {
 		<>
 		<div className="splashPic"></div>
 			<h2>Home Page</h2>
+
+
+			<h4> random mumbo jumbo</h4>
+
+			<div className="review">
+			Review
+			</div>
+			<div className="review">
+			Review
+			</div>
+			<div className="review">
+			Review
+			</div>
 		</>
+		
+		
 	)
+
+	
 }
 
 export default Home
